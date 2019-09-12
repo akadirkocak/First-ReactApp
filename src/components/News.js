770d5329576a8edb9301757dd094c77db6978442
Abdulkadir KOÇAK <kadir.kocak.50@hotmail.com>
2019-09-12 22:40:11 +0200

    Edit and added

---
+++ src/components/News.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NewsItem from "./NewsItem";
+
+class News  extends React.Component{
+    render() {
+        return(
+            <div>
+                <NewsItem></NewsItem>
+            </div>
+        )
+    }
+}
